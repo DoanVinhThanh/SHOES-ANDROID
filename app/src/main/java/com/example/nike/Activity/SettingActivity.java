74db3cfeee74f9410c0b506649666f3819101f40
thanhnhut2311 <130993688+thanhnhut2311@users.noreply.github.com>
2024-08-19 15:05:26 +0700

    Update setting va favorite

--- app/src/main/java/com/example/nike/Activity/SettingActivity.java
+++ app/src/main/java/com/example/nike/Activity/SettingActivity.java
@@ -2,15 +2,36 @@ package com.example.nike.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.nike.R;
 
 public class SettingActivity extends AppCompatActivity {
 
+    ImageView btn_setting_shop_to_home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        Anhxa();
+        btn_setting_shop_to_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingActivity.this, TrangChuActivity.class);
+                startActivity(intent);
+                
+
+            }
+        });
+
+    }
+    private void Anhxa()
+    {
+        btn_setting_shop_to_home = findViewById(R.id.btn_setting_shop_to_home);
     }
 }
+
