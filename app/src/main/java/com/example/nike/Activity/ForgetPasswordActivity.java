cdfea0a78d06a0342598874b50f8ecfd172d6996
DoanVinhThanh <doanvinhthanhcc@gmail.com>
2025-02-15 11:24:08 +0700

    update sign in sign up forget password

--- app/src/main/java/com/example/nike/Activity/ForgetPasswordActivity.java
+++ app/src/main/java/com/example/nike/Activity/ForgetPasswordActivity.java
@@ -1,18 +1,40 @@
 package com.example.nike.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.nike.R;
 
 public class ForgetPasswordActivity extends AppCompatActivity {
-
+    TextView btnForgotSignIn, btnForgotSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+        Anhxa();
+        btnForgotSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ForgetPasswordActivity.this,SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnForgotSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ForgetPasswordActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 
+    public void Anhxa() {
+        btnForgotSignIn = findViewById(R.id.btn_forgot_signin);
+        btnForgotSignUp = findViewById(R.id.btn_forgot_signup);
 
     }
 }
--- app/src/main/java/com/example/nike/Activity/SignInActivity.java
+++ app/src/main/java/com/example/nike/Activity/SignInActivity.java
@@ -1,21 +1,42 @@
 package com.example.nike.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.example.nike.R;
 
 public class SignInActivity extends AppCompatActivity {
-
-
+    TextView btnSignInForgot , btnSignInSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_sign_in);
-
+        Anhxa();
+        btnSignInForgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInActivity.this, ForgetPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnSignInSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public void Anhxa(){
+        btnSignInForgot = findViewById(R.id.btn_signin_forgot);
+        btnSignInSignUp = findViewById(R.id.btn_signin_signup);
     }
 }
